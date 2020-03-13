803706af085d51b4116772d5c2cadf789f42f01b
JeannieM <bean.mccarthy@gmail.com>
2020-03-13 10:07:26 -0400

    first commit

---
+++ src/main/java/co/grandcircus/WatchYourBackpack/WatchYourBackpackApplication.java
@@ -0,0 +1,13 @@
+package co.grandcircus.WatchYourBackpack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WatchYourBackpackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WatchYourBackpackApplication.class, args);
+	}
+
+}
