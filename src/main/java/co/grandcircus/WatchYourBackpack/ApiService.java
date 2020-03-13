e44cd5b7f1349cbdbd7789052424e668095e21c8
sean <s.warchuck16@gmail.com>
2020-03-13 10:33:21 -0400

    first commit made a ApiService

---
+++ src/main/java/co/grandcircus/WatchYourBackpack/ApiService.java
@@ -0,0 +1,24 @@
+package co.grandcircus.WatchYourBackpack;
+
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.client.ClientHttpRequestInterceptor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+@Component
+public class ApiService {
+	
+	private RestTemplate rt;
+
+	// initialization block that runs when a new instance of the class is created
+	// loaded before the constructor
+	{
+		ClientHttpRequestInterceptor interceptor = (request, body, execution) -> {
+			request.getHeaders().add(HttpHeaders.USER_AGENT, "ooeeee!");
+			return execution.execute(request, body);
+		};
+		rt = new RestTemplateBuilder().additionalInterceptors(interceptor).build();
+	}
+
+}
