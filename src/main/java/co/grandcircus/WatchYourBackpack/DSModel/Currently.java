c0f184cedd355abacf13173f91e618deb0856c59
JeannieM <bean.mccarthy@gmail.com>
2020-03-13 10:37:02 -0400

    added weather model

---
+++ src/main/java/co/grandcircus/WatchYourBackpack/DSModel/Currently.java
@@ -0,0 +1,51 @@
+package co.grandcircus.WatchYourBackpack.DSModel;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+
+@Entity
+public class Currently {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id; 	
+	private String summary;
+	private String icon;
+	private Double temperature;
+	
+	//rework the below to work with National Parks?
+	//@OneToOne(mappedBy = "weather")
+	//private FavEvent favEvent;
+	
+	
+	
+	public String getSummary() {
+		return summary;
+	}
+	public void setSummary(String summary) {
+		this.summary = summary;
+	}
+	public String getIcon() {
+		return icon;
+	}
+	public void setIcon(String icon) {
+		this.icon = icon;
+	}
+	public Double getTemperature() {
+		return temperature;
+	}
+	public void setTemperature(Double temperature) {
+		this.temperature = temperature;
+	}
+	
+	@Override
+	public String toString() {
+		return "Currently [summary=" + summary + ", icon=" + icon + ", temperature=" + temperature + "]";
+	}
+
+	
+	
+}
