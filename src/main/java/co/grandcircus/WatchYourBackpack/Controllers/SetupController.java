9534c52b3f8c822594c28acd4dfb8444ba0da007
JeannieM <bean.mccarthy@gmail.com>
2020-03-23 16:54:41 -0400

    redirect errors

--- src/main/java/co/grandcircus/WatchYourBackpack/Controllers/SetupController.java
+++ src/main/java/co/grandcircus/WatchYourBackpack/Controllers/SetupController.java
@@ -11,6 +11,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import co.grandcircus.WatchYourBackpack.DSApiService;
 import co.grandcircus.WatchYourBackpack.ParksService;
@@ -68,7 +69,7 @@ public class SetupController {
 //	}
 
 	@RequestMapping("/")
-	public ModelAndView showHome() {
+	public ModelAndView showHome(RedirectAttributes rd) {
 		ModelAndView mav = new ModelAndView("index");
 
 		// grabbing the list of players from the database
@@ -88,27 +89,28 @@ public class SetupController {
 	}
 
 	@PostMapping("/newPlayer")
-	public ModelAndView addNewPlayer(String name, String description, Integer type) {//, Double money this is default for new players
-
+	public ModelAndView addNewPlayer(String name, String description, Integer type) {
+		
 		playerService.createPlayer(name, description, type);
 
 		return new ModelAndView("redirect:/");
 	}
 
 	@PostMapping("/start")
-	public ModelAndView startGame(String parkCodeName, String parkCodeState, String parkCodeFee, Long id) {
+	public ModelAndView startGame(String parkCodeName, String parkCodeState, String parkCodeFee, Long id, RedirectAttributes rd) {
 		ModelAndView mav = new ModelAndView("start");
 
 		Player chosenPlayer = playerDao.findById(id).orElse(null);
 		if (chosenPlayer.equals(null)) {
-			return new ModelAndView("redirect:/", "message", "No player was selected. Please choose or create a player first!");
+			return new ModelAndView("redirect:/", "noPlayerMessage", "No player was selected. Please choose or create a player first!");
 		}
 		
 		String parkCode = parksService.determineParkCode(parkCodeName, parkCodeState, parkCodeFee);
-		if (parkCode.equals("none")) {		
-			return new ModelAndView("redirect:/", "message", "No park chosen. Please choose a park!");
+		if (parkCode.equals("none")) {	
+			rd.addFlashAttribute("parkMessage", "No park chosen. Please choose a park!");
+			return new ModelAndView("redirect:/");
 		}else if (parkCode.equals("many")) {
-			return new ModelAndView("redirect:/", "message", "Okay, it's virtual, but it's not THAT virtual. You can't be in two places at once! Please choose just one park!");
+			return new ModelAndView("redirect:/", "parkMessage", "Okay, it's virtual, but it's not THAT virtual. You can't be in two places at once! Please choose just one park!");
 		}
 		
 		DBPark park = pDao.findByParkCodeContaining(parkCode);	
